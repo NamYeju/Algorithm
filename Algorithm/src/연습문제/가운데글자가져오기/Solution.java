250ae4481056f605e9e279276c4aab9451d17b27
NamYeju <dpwn0112@naver.com>
2021-01-03 00:12:58 +0900

    프로그래머스/ 연습문제 / level1

---
+++ Algorithm/src/연습문제/가운데글자가져오기/Solution.java
@@ -0,0 +1,30 @@
+package 연습문제.가운데글자가져오기;
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		String s;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("s값을 입력하세요");
+		s = sc.nextLine();
+		System.out.print(solution(s));
+	}
+	
+    public static String solution(String s) {
+    	String answer = "";
+    	
+    	if(s.length()%2==0) {
+    		
+    		answer = s.substring(s.length()/2-1,s.length()/2+1);
+    	
+    	}
+    	if(s.length()%2==1) {
+    		answer = s.substring(s.length()/2,s.length()/2+1);
+        	
+    	}
+        
+        return answer;
+    }
+}
