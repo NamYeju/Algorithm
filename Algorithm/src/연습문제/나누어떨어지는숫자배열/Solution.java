f34d060a172bb7d7bcaf08af609110e57d0dd04c
NamYeju <dpwn0112@naver.com>
2021-01-07 00:16:42 +0900

    프로그래머스/ 연습문제 / level1

--- Algorithm/src/연습문제/나누어떨어지는숫자배열/Solution.java
+++ Algorithm/src/연습문제/나누어떨어지는숫자배열/Solution.java
@@ -1,17 +1,17 @@
 package 연습문제.나누어떨어지는숫자배열;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Solution {
 	public static void main(String[] args) {
 		int arr[] = {5,9,7,10};
-		
-		System.out.print("asdf");
+
 		System.out.print(solution(arr,5));
 	}
     public static int[] solution(int[] arr, int divisor) {
-    	
+    	int[] answer = {};
        
         ArrayList<Integer> arr2 = new ArrayList<Integer>();
         
@@ -21,16 +21,20 @@ public class Solution {
         	}
         }
  
-        	int[] answer = new int[arr2.size()];
-        
-        	if(arr2.size()==0) {
-        		answer[0] = -1;
-        	}
+        if(arr2.isEmpty()) {
+        	answer = new int[1];
+        	answer[0] = -1;
+        }
+        else {
+        	answer = new int[arr2.size()];
 	        for(int i=0; i<arr2.size(); i++) {
 	        	answer[i] = arr2.get(i).intValue();
 	        	}
-        
-        
+	        Arrays.sort(answer);
+        }
+        	
+  
+
         return answer;
     }
 }
