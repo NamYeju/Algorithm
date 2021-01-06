159e0dd1bfadc5399d135be2b53ce09df40044a2
NamYeju <dpwn0112@naver.com>
2021-01-07 00:25:52 +0900

    프로그래머스 / 연습문제 / level1

--- Algorithm/src/연습문제/같은숫자는싫어/Solution.java
+++ Algorithm/src/연습문제/같은숫자는싫어/Solution.java
@@ -13,19 +13,25 @@ public class Solution {
     public static int[] solution(int []arr) {
         
         ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        
         for(int i=1; i<arr.length; i++) {
         	
         	if(arr[i-1] != arr[i]) {
         		arr2.add(arr[i-1]);
         	}
+        	if(i == arr.length-1) {
+        		arr2.add(arr[i]);
+        	}
         	
         }
+        
         int[] answer = new int[arr2.size()];
-        for(int i=0;i<arr2.size();i++) {
-        	answer[i] = arr2.get(i);
+        
+        for(int i=0; i<arr2.size(); i++) {
+        	answer[i] = arr2.get(i).intValue();
         }
    
-        System.out.println("Hello Java");
+
 
         return answer;
     }
