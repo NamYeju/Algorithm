8dac3d84d9ed0fdd7808eb50ce874385d1f1f188
Nam Yeju <dpwn0112@naver.com>
2021-04-16 22:50:47 +0900

    백준알고리즘 / 문자열 / 1212번 / 미완성

--- Algorithm/src/백준알고리즘/문자열/_8진수2진수/Main.java
+++ Algorithm/src/백준알고리즘/문자열/_8진수2진수/Main.java
@@ -1,5 +1,6 @@
 package 백준알고리즘.문자열._8진수2진수;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,21 +11,22 @@ public class Main {
 		int a = 0;
 		ArrayList<String> arr = new ArrayList<String>();
 		num = sc.next();
-
-		for(int i=0; i< num.length(); i--) {
+		for(int i=0; i< num.length(); i++) {
 			//5-> 2...1 -> 1...0 
-			a = (int)num.charAt(i);
-			while(num.charAt(i)/2 == 1) {
-				if((num.charAt(i))%2==0) {
+			a = num.charAt(i);
+			while(a == 1) {
+				if(a%2==0) {
 					arr.add("0");
 				}
-				else if((num.charAt(i))%2==1) {
+				else if(a%2==1) {
 					arr.add("1");
 				}	
 				a /= 2;
 				System.out.println(a);
 			}
 		}
+		
+		System.out.println(arr.toString());
 		
 
 	}
