a734fad7cad34164b48cbee8fad4b1ddee2c1ece
Nam Yeju <dpwn0112@naver.com>
2021-04-18 23:56:45 +0900

    백준알고리즘 / 문자열 / 1212번

--- Algorithm/src/백준알고리즘/문자열/_8진수2진수/Main.java
+++ Algorithm/src/백준알고리즘/문자열/_8진수2진수/Main.java
@@ -1,33 +1,43 @@
 package 백준알고리즘.문자열._8진수2진수;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		String num;
-		int a = 0;
-		ArrayList<String> arr = new ArrayList<String>();
+		String str3="";
+		String answer="";
+		String a ="";
 		num = sc.next();
+		String[] str = new String[num.length()];
+
 		for(int i=0; i< num.length(); i++) { //숫자 한자리씩 분리 
-			//분리한 숫자 2진수로 변환하기 5-> 2...1 -> 1...0 
-			a = num.charAt(i);
-			while(a != 1) {
-				if(a%2==0) {
-					arr.add("0");
+			str3 = "";
+			a = "";
+			a = Integer.toBinaryString(num.charAt(i)-'0');
+			//str[i]= Integer.toBinaryString(a);
+			if(a.length()!=3) {
+				if(a.length()==1) { 
+					str3 += "00";
+					str3 += a;
+					answer += str3;
 				}
-				else if(a%2==1) {
-					arr.add("1");
-				}	
-				a /= 2;
-				System.out.println(a);
+				if(a.length()==2) { 
+					str3 += "0";
+					str3 += a;
+					answer += str3;
+				}
+				
 			}
+			else answer += a;
+
 		}
-
-		System.out.println(arr);
 		
-
+		if(answer.substring(0,1).equals("0")) {
+			answer=answer.substring(1,answer.length());
+		}
+		System.out.println(answer);
 	}
 
 }
