77adaca288b0680f458f35f141784832be14079f
Nam Yeju <dpwn0112@naver.com>
2021-05-27 00:02:42 +0900

    프로그래머스 / 연습문제 / level1

--- Algorithm/src/프로그래머스/연습문제/핸드폰번호가리기/Solution.java
+++ Algorithm/src/프로그래머스/연습문제/핸드폰번호가리기/Solution.java
@@ -1,5 +1,12 @@
 package 프로그래머스.연습문제.핸드폰번호가리기;
 
-public class Solution {
-
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        for(int i=0; i<phone_number.length()-4; i++) {
+        	answer+="*";
+        }
+        answer += phone_number.substring(phone_number.length()-4, phone_number.length());
+        return answer;
+    }
 }
