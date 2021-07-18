0cc9640e4c1dd72a24af0c770d8622ee2722e52d
Nam Yeju <dpwn0112@naver.com>
2021-07-19 01:00:17 +0900

    프로그래머스 / 연습문제 / level1

--- Algorithm/src/프로그래머스/연습문제/자연수뒤집어배열로만들기/Solution.java
+++ Algorithm/src/프로그래머스/연습문제/자연수뒤집어배열로만들기/Solution.java
@@ -1,5 +1,14 @@
 package 프로그래머스.연습문제.자연수뒤집어배열로만들기;
 
-public class Solution {
-
+class Solution {
+    public int[] solution(long n) {
+        int[] answer = {};
+        String str = String.valueOf(n);
+        answer = new int[str.length()];
+        
+        for(int i = str.length()-1; i>=0; i--) {
+        	answer[str.length()-(i+1)] = Character.getNumericValue(str.charAt(i));
+        }
+        return answer;
+    }
 }
--- Algorithm/src/프로그래머스/연습문제/정수제곱근판별/Solution.java
+++ Algorithm/src/프로그래머스/연습문제/정수제곱근판별/Solution.java
@@ -1,5 +1,16 @@
 package 프로그래머스.연습문제.정수제곱근판별;
 
-public class Solution {
-
-}
+class Solution {
+	public long solution(long n) {
+		long answer = 0;
+		for(long i = 0; i <= n; i++){
+			if(i*i == n){
+				answer = (i + 1)*(i+1);
+				break;
+				}
+			else
+				answer = -1;
+			}
+		return answer;
+		}
+	}
