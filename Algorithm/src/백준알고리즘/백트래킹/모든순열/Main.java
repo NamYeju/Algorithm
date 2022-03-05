9c3a634a12751d3f12cbf96644d73eb43c62a364
NamYeju <dpwn0112@naver.com>
2022-03-05 23:58:37 +0900

    백준 / 백트래킹 / 10974번

---
+++ Algorithm/src/백준알고리즘/백트래킹/모든순열/Main.java
@@ -0,0 +1,9 @@
+package Algorithm.src.백준알고리즘.백트래킹.모든순열;
+
+public class Main {
+    public static void main(String args[]){
+    }
+    static void solution(int depth, boolean[] visited, int n){
+
+    }
+}
