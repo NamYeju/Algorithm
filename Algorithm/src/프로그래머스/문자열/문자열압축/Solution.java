1b458c55967ded69458d252999ae80f62d36c517
NamYeju <dpwn0112@naver.com>
2023-05-02 00:47:15 +0900

    프로그래머스 / level2

--- Algorithm/src/프로그래머스/문자열/문자열압축/Solution.java
+++ Algorithm/src/프로그래머스/문자열/문자열압축/Solution.java
@@ -1,29 +1,47 @@
 package Algorithm.src.프로그래머스.문자열.문자열압축;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 	public int solution(String s) {
 		int answer = s.length();
 		for(int i = 1; i <= s.length(); i++){
 			List<String> tempList = token(i, s);
-			System.out.println(tempList);
+			answer = Math.min(answer,result(tempList));
 		}
 		return answer;
 	}
 	public List<String> token(int n, String s){
 		List<String> tokenList = new ArrayList<>();
-		int cnt = 0;
-		for(int i = 0; i + n  <= s.length(); i = i + n){
-			tokenList.add(s.substring(i, i + n));
-			cnt++;
+		int lastIndex = 0;
+		for(int startIndex = 0; startIndex < s.length(); startIndex = lastIndex){
+			lastIndex = startIndex + n;
+			if(lastIndex > s.length() - 1)
+				lastIndex = s.length();
+			tokenList.add(s.substring(startIndex, lastIndex));
 		}
-		// 알맞게 토큰 자른 후 나머지 토큰
-		if(cnt * n < s.length()){
-			tokenList.add(s.substring(cnt * n - 1));
-		}
-
 		return tokenList;
+	}
+	public int result(List<String> tokenList){
+		StringBuilder sb = new StringBuilder();
+		int cnt = 0;
+		String last = "";
+		for(int i = 0; i < tokenList.size(); i++){
+			String now = tokenList.get(i);
+			if(now.equals(last)){
+				cnt++;
+			}
+			else {
+				if(cnt > 1){
+					sb.append(cnt);}
+				sb.append(last);
+				last = now;
+				cnt = 1;
+			}
+		}
+		if(cnt > 1)
+			sb.append(cnt);
+		sb.append(last);
+		return sb.toString().length();
 	}
 }
