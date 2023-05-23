39f20d89005f1979d021117b546f896d31af9b8e
NamYeju <dpwn0112@naver.com>
2023-05-23 15:54:19 +0900

    프로그래머스 / level2

---
+++ Algorithm/src/프로그래머스/배열/행렬의곱셈/Solution.java
@@ -0,0 +1,18 @@
+package Algorithm.src.프로그래머스.배열.행렬의곱셈;
+
+public class Solution {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+
+		for(int i = 0; i < arr1.length; i++){
+			for(int j = 0; j < arr2[0].length; j++){
+				answer[i][j] = 0;
+				for(int k = 0; k < arr1[0].length; k++){
+					answer[i][j] += arr1[i][k] * arr2[k][j];
+				}
+			}
+		}
+
+		return answer;
+	}
+}
