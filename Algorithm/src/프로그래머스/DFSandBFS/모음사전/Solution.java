fcf18467801a9f46064ff25876265a6c678827fe
NamYeju <dpwn0112@naver.com>
2023-05-29 23:31:41 +0900

    프로그래머스 / level2

--- Algorithm/src/프로그래머스/DFSandBFS/모음사전/Solution.java
+++ Algorithm/src/프로그래머스/DFSandBFS/모음사전/Solution.java
@@ -1,20 +1,29 @@
 package Algorithm.src.프로그래머스.DFSandBFS.모음사전;
 
-import java.util.*;
 public class Solution {
-	static String[] alphabet = {"A", "E", "I", "O", "U"};
-	static List<String> list = new ArrayList<>();
+	static char[] alphabet = {'A', 'E', 'I', 'O', 'U'};
+	static int cnt;
+	static int answer;
 	public int solution(String word) {
-		dfs("", 0);
-		int answer = list.indexOf(word);
+		cnt = 0;
+		answer = 0;
+		dfs(word,"", 0);
 		return answer;
 	}
-	public void dfs(String now, int depth){
-		list.add(now);
-		if(depth == 5) return;
-		for(int i = 0; i < 4; i++){
-			dfs(now + alphabet[i], depth+1);
+	public void dfs(String word, String str, int depth){
+		if(str.equals(word)){
+			answer = cnt;
+			return;
+		}
+		if(depth == 5) {
+			cnt++;
+			return;
+		}
+		cnt++;
+		for(int i = 0; i < alphabet.length; i++){
+			dfs(word, str + alphabet[i], depth+1);
 		}
 	}
 }
+
 
